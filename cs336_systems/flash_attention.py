aaf8ccbd44b27032319d0bb94c4c5f61d6b19b26
Ziyi Chen <zy035c@gmail.com>
2026-02-02 01:01:50 +0800

    flash atten init

---
+++ cs336_systems/flash_attention.py
@@ -0,0 +1,39 @@
+import triton
+import triton.language as tl
+
+@triton.jit
+def flash_attention_forward_kernel(
+    Q_ptr, K_ptr, V_ptr,
+    O_ptr, L_ptr,
+    stride_qb, stride_qq, stride_qd,
+    stride_kb, stride_kk, stride_kd,
+    stride_vb, stride_vk, stride_vd,
+    stride_ob, stride_oq, stride_od,
+    stride_lb, stride_lq,
+    N_QUERIES, N_KEYS,
+    scale,
+    D: tl.constexpr,
+    Q_TILE_SIZE: tl.constexpr,
+    K_TILE_SIZE: tl.constexpr,
+):
+    query_tile_index = tl.program_id(0)
+    batch_index = tl.program_id(1)
+
+    # Offset each pointer with the corresponding batch index
+    # multiplied with the batch stride for each tensor
+    Q_block_ptr = tl.make_block_ptr(
+        Q_ptr + batch_index * stride_qb,
+        shape=(N_QUERIES, D),
+        strides=(stride_qq, stride_qd),
+        offsets=(query_tile_index * Q_TILE_SIZE, 0),
+        block_shape=(Q_TILE_SIZE, D),
+        order=(1, 0),
+    )
+    pass
+
+
+class FlashAttention2:
+
+    @staticmethod
+    def forward(ctx, Q, K, V, is_causal=False):
+        pass

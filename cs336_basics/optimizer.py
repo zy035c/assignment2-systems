d663b3d97a6b96ba75286dc745514c3121784ff4
Ziyi Chen <zy035c@gmail.com>
2026-02-02 02:10:35 +0800

    deving flash attn

--- cs336_basics/optimizer.py
+++ cs336_basics/optimizer.py
@@ -1,33 +1,30 @@
-from jaxtyping import Float, Int
+from __future__ import annotations
+
+import math
+from collections.abc import Callable, Iterable
+
 import torch
 
-from collections.abc import Callable, Iterable
-from typing import Optional
-import math
 
-
-class SGD(torch.optim.Optimizer):
-    def __init__(self, params, lr=1e-3):
-        if lr < 0:
-            raise ValueError(f"Invalid learning rate: {lr}")
-        defaults = {"lr": lr}
-        super().__init__(params, defaults)
-
-    def step(self, closure: Optional[Callable] = None):
-        loss = None if closure is None else closure()
-        for group in self.param_groups:
-            lr = group["lr"]  # Get the learning rate.
-
-            for p in group["params"]:
-                if p.grad is None:
-                    continue
-
-                state = self.state[p]
-                t = state.get("t", 0)
-                grad = p.grad.data
-                p.data -= lr * math.sqrt(t + 1) * grad
-                state["t"] = t + 1
-        return loss
+def get_cosine_lr(
+    it: int,
+    max_learning_rate: float,
+    min_learning_rate: float,
+    warmup_iters: int,
+    cosine_cycle_iters: int,
+):
+    """Cosine with warmup learning rate scheduler."""
+    # First, we linearly warmup for warmup_iters steps.
+    if it < warmup_iters:
+        return max_learning_rate * it / warmup_iters
+    # Then, if it > cosine_cycle_iters, we return min learning rate.
+    if it > cosine_cycle_iters:
+        return min_learning_rate
+    # Else, we use cosine decay down to min learning rate.
+    decay_ratio = (it - warmup_iters) / (cosine_cycle_iters - warmup_iters)
+    assert 0 <= decay_ratio <= 1
+    coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio))
+    return min_learning_rate + coeff * (max_learning_rate - min_learning_rate)
 
 
 class AdamW(torch.optim.Optimizer):
@@ -59,61 +56,31 @@ class AdamW(torch.optim.Optimizer):
                 if p.grad is None:
                     continue
 
-                state = self.state[p]
-                t = state.get("t", 1)
+                # Can either apply weight decay here, or at the very end
+                # p.data.mul_(1 - group['lr'] * group['weight_decay'])
+
                 grad = p.grad.data
+                if grad.is_sparse:
+                    raise RuntimeError("Adam does not support sparse gradients")
 
-                m = state.get("m", torch.zeros_like(p.data))
-                v = state.get("v", torch.zeros_like(p.data))
-                m = beta1 * m + (1 - beta1) * grad
-                v = beta2 * v + (1 - beta2) * (grad * grad)
+                state = self.state[p]
+                alpha = group["lr"]
+                beta_1, beta_2 = group["betas"]
+                eps = group["eps"]
+                t = state.get("t", 1)
+                prev_m_t = state.get("m", torch.zeros_like(grad))
+                prev_v_t = state.get("v", torch.zeros_like(grad))
 
-                lr_t = lr * math.sqrt(1 - beta2**(t)) / (1 - beta1**(t))
+                m_t = beta_1 * prev_m_t + ((1 - beta_1) * grad)
+                v_t = beta_2 * prev_v_t + ((1 - beta_2) * torch.square(grad))
 
-                p.data -= lr_t * (m / (torch.sqrt(v) + eps))
-                p.data -= lr * weight_decay * p.data
+                alpha_t = alpha * (math.sqrt(1 - (beta_2**t)) / (1 - (beta_1**t)))
+                p.data -= alpha_t * m_t / (torch.sqrt(v_t) + eps)
+                # Apply weight decay
+                p.data -= alpha * group["weight_decay"] * p.data
+
+                state["m"] = m_t
+                state["v"] = v_t
                 state["t"] = t + 1
-                state["m"] = m
-                state["v"] = v
 
         return loss
-
-
-def lr_cosine_schedule(t, lr_max, lr_min, Tw, Tc):
-    if t < Tw:
-        return t / Tw * lr_max
-    elif Tw <= t <= Tc:
-        return lr_min + .5 * (lr_max - lr_min) * (1 + math.cos(math.pi * (t - Tw) / (Tc - Tw)))
-    else:
-        return lr_min
-
-from torch.optim.lr_scheduler import _LRScheduler
-
-class CosineWarmupScheduler(_LRScheduler):
-    def __init__(self, optimizer, lr_max, lr_min, Tw, Tc, last_epoch=-1):
-        self.lr_max = lr_max
-        self.lr_min = lr_min
-        self.Tw = Tw
-        self.Tc = Tc
-        super().__init__(optimizer, last_epoch)
-
-    def get_lr(self):
-        t = self.last_epoch
-        lr = lr_cosine_schedule(t, self.lr_max, self.lr_min, self.Tw, self.Tc)
-        return [lr for _ in self.optimizer.param_groups]
-
-
-def gradient_clipping(
-    inputs: Iterable[torch.nn.Parameter],
-    M: float,
-    eps=1e-6
-):
-    l2_norm = torch.norm(
-        torch.stack([p.grad.detach().norm(2) for p in inputs if p.grad is not None]),
-        2
-    )
-    for p in inputs:
-        if p.grad is None: continue
-        if l2_norm >= M:
-            p.grad = p.grad * (M / (l2_norm + eps))
-    pass
--- cs336_systems/flash_attention.py
+++ cs336_systems/flash_attention.py
@@ -1,5 +1,49 @@
+import math
+import torch
 import triton
 import triton.language as tl
+
+
+class FlashAttentionWoTrion(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, Q: torch.Tensor, K: torch.Tensor, V: torch.Tensor, is_causal=False):
+
+        dtype = Q.dtype
+        device = Q.device
+        N_QUERIES, D = Q.shape[-2], Q.shape[-1]
+        N_KEYS, _ = K.shape[-2], K.shape[-1]
+
+        Bq, Bk = 64, 64  # Tile sizes
+
+        Tq = math.ceil(N_QUERIES / Bq)
+        Tk = math.ceil(N_KEYS / Bk)
+
+        other_dims = Q.shape[:-2]
+        O = torch.zeros_like((*other_dims, N_QUERIES, D), device=device, dtype=dtype)
+        L = torch.zeros_like((*other_dims, N_QUERIES), device=device, dtype=dtype)
+
+        for i in range(Tq):  # loop for program instances
+            Qi_upper_bound = (i + 1) * Bq if (i + 1) * Bq < N_QUERIES else N_QUERIES  # padding
+            q_tile = Q[..., i * Bq: Qi_upper_bound, :]
+            Oi = O[..., i * Bq: Qi_upper_bound, :]
+
+            actual_Bq = q_tile.shape[-2]  # because of padding
+
+            li = torch.zeros((*other_dims, actual_Bq,), device=device, dtype=dtype)
+            mi = torch.full((*other_dims, actual_Bq,), fill_value=-torch.inf, device=device, dtype=dtype)
+
+            for j in range(Tk):  # loop of a single program
+                Kj_upper_bound = (j + 1) * Bk if (j + 1) * Bk < N_KEYS else N_KEYS  # padding
+                k_tile = K[..., j * Bk: Kj_upper_bound, :]
+                Vj = V[..., j * Bk: Kj_upper_bound, :]
+
+                k_tile = k_tile.transpose(-1, -2)  # transpose for matmul
+                Sij = q_tile @ k_tile / math.sqrt(D)  # [*, Bq, Bk]
+
+                Sij_rowmax = torch.max(Sij, dim=-1, keepdim=False).values
+
+
+
 
 @triton.jit
 def flash_attention_forward_kernel(
@@ -11,10 +55,10 @@ def flash_attention_forward_kernel(
     stride_ob, stride_oq, stride_od,
     stride_lb, stride_lq,
     N_QUERIES, N_KEYS,
-    scale,
+    scale,  #  = 1/sqrt(d)
     D: tl.constexpr,
-    Q_TILE_SIZE: tl.constexpr,
-    K_TILE_SIZE: tl.constexpr,
+    Q_TILE_SIZE: tl.constexpr,  # Bq
+    K_TILE_SIZE: tl.constexpr,  # Bk
 ):
     query_tile_index = tl.program_id(0)
     batch_index = tl.program_id(1)
@@ -22,18 +66,40 @@ def flash_attention_forward_kernel(
     # Offset each pointer with the corresponding batch index
     # multiplied with the batch stride for each tensor
     Q_block_ptr = tl.make_block_ptr(
-        Q_ptr + batch_index * stride_qb,
+        base=Q_ptr + batch_index * stride_qb,
         shape=(N_QUERIES, D),
         strides=(stride_qq, stride_qd),
         offsets=(query_tile_index * Q_TILE_SIZE, 0),
         block_shape=(Q_TILE_SIZE, D),
         order=(1, 0),
     )
-    pass
 
+    K_block_ptr = tl.make_block_ptr(
+        base=K_ptr + batch_index * stride_kb,
+        shape=(N_KEYS, D),
+        strides=(stride_kk, stride_kd),
+        offsets=(0, 0),
+        block_shape=(D, K_TILE_SIZE),
+    )
+
+    Tq = triton.cdiv(N_QUERIES, Q_TILE_SIZE)
+    Tk = triton.cdiv(N_KEYS, K_TILE_SIZE)
+
+    q_tile = tl.load(Q_block_ptr, boundary_check=(0, 1), padding_option="zero")
+
+    for j in range(Tk):
+        k_tile = tl.load(K_block_ptr, boundary_check=(0, 1), padding_option="zero")
+
+        # Compute attention scores
+        
 
 class FlashAttention2:
 
     @staticmethod
     def forward(ctx, Q, K, V, is_causal=False):
+
+        flash_attention_forward_kernel[()]()
+
+    @staticmethod
+    def backward(ctx, grad_o: torch.Tensor):
         pass
